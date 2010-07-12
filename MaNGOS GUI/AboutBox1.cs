efc75371136f1625851d2824064d79595a7026af
JustZombe <ndolgushin@ya.ru>
2010-07-12 18:41:16 +0600

    [4]Fixed "ok" button in AboutBox.

--- MaNGOS GUI/AboutBox1.cs
+++ MaNGOS GUI/AboutBox1.cs
@@ -100,5 +100,10 @@ namespace MaNGOS_GUI
             }
         }
         #endregion
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
